2fab3adbf789bd88e3196805adbbc1e45cbc741a
Etienne Dilocker <etienne@dilocker.de>
2022-05-12 17:36:50 +0200

    calculate recall @10 and @100

--- benchmark-scripts/src/functions.py
+++ benchmark-scripts/src/functions.py
@@ -25,7 +25,7 @@ def handle_results(results):
                     loguru.logger.info(message['message'])
 
 
-def match_results(test_set, weaviate_result_set):
+def match_results(test_set, weaviate_result_set, k):
     '''Match the reults from Weaviate to the benchmark data.
        If a result is in the returned set, score goes +1.
        Because there is checked for 100 neighbors a score
@@ -40,11 +40,11 @@ def match_results(test_set, weaviate_result_set):
 
     # create array from Weaviate result
     weaviate_result_array = []
-    for weaviate_result in weaviate_result_set['data']['Get']['Benchmark']:
+    for weaviate_result in weaviate_result_set['data']['Get']['Benchmark'][:k]:
         weaviate_result_array.append(weaviate_result['counter'])
 
     # match scores
-    for nn in test_set:
+    for nn in test_set[:k]:
         if nn in weaviate_result_array:
             score += 1
     
@@ -70,10 +70,17 @@ def conduct_benchmark(weaviate_url, CPUs, ef, client, benchmark_file, efConstruc
         'ef': ef,
         'efConstruction': efConstruction,
         'maxConnections': maxConnections,
-        'score': {
-            'highest': 0,
-            'lowest': 100,
-            'average': 0
+        'recall': {
+            '100': {
+                'highest': 0,
+                'lowest': 100,
+                'average': 0
+            },
+            '10': {
+                'highest': 0,
+                'lowest': 100,
+                'average': 0
+            },
         },
         'requestTimes': {}
     }
@@ -86,7 +93,11 @@ def conduct_benchmark(weaviate_url, CPUs, ef, client, benchmark_file, efConstruc
     # Run the score test
     ##
     c = 0
-    all_scores = []
+    all_scores = {
+            '100':[],
+            '10':[],
+        }
+
     loguru.logger.info('Find neighbors with ef = ' + str(ef))
     with h5py.File('/var/hdf5/' + benchmark_file[0], 'r') as f:
         test_vectors = f['test']
@@ -98,16 +109,19 @@ def conduct_benchmark(weaviate_url, CPUs, ef, client, benchmark_file, efConstruc
             
             # Start request
             query_result = client.query.get("Benchmark", ["counter"]).with_near_vector(nearVector).with_limit(100).do()    
-            score = match_results(f['neighbors'][c], query_result)
-            if score == 0:
-                loguru.logger.info('There is a 0 score, this most likely means there is an issue with the dataset OR you have very low index settings. Found for vector: ' + str(test_vector[0]))
-            all_scores.append(score)
-            
-            # set if high and low score
-            if score > results['score']['highest']:
-                results['score']['highest'] = score
-            if score < results['score']['lowest']:
-                results['score']['lowest'] = score
+
+            for k in [10,100]:
+                k_label=f'{k}'
+                score = match_results(f['neighbors'][c], query_result, k)
+                if score == 0:
+                    loguru.logger.info('There is a 0 score, this most likely means there is an issue with the dataset OR you have very low index settings. Found for vector: ' + str(test_vector[0]))
+                all_scores[k_label].append(score)
+                
+                # set if high and low score
+                if score > results['recall'][k_label]['highest']:
+                    results['recall'][k_label]['highest'] = score
+                if score < results['recall'][k_label]['lowest']:
+                    results['recall'][k_label]['lowest'] = score
 
             # log ouput
             if (c % 1000) == 0:
@@ -135,7 +149,8 @@ def conduct_benchmark(weaviate_url, CPUs, ef, client, benchmark_file, efConstruc
     # add final results
     results['totalTested'] = c
     results['totalDatasetSize'] = train_vectors_len
-    results['score']['average'] = sum(all_scores) / len(all_scores)
+    for k in ['10', '100']:
+        results['recall'][k]['average'] = sum(all_scores[k]) / len(all_scores[k])
 
     return results
 
@@ -155,7 +170,7 @@ def import_into_weaviate(client, efConstruction, maxConnections, benchmark_file)
     '''Imports the data into Weaviate'''
     
     # variables
-    benchmark_import_batch_size = 1000
+    benchmark_import_batch_size = 10000
     benchmark_class = 'Benchmark'
     import_time = 0
 
