0291c9cb8aa06e9a0d370a9c39a4275bdd51fd49
Stefan Bogdan <stefanbogdan180394@gmail.com>
2022-05-30 16:58:07 +0200

    fix index bug

--- benchmark-scripts/src/functions.py
+++ benchmark-scripts/src/functions.py
@@ -442,7 +442,7 @@ def import_data_into_weaviate(
                 results = []
                 for i in range(nr_cores):
                     current_index = i + proc_batch * nr_cores
-                    if current_index == nr_vectors:
+                    if current_index == nr_processes:
                         break
                     results.append(
                         executor.submit(
