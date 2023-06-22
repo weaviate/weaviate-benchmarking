e0cf7fd2399995c3cd02945d19f305e82fe4e656
George Williams <george.williams@gmail.com>
2023-06-22 00:49:36 +0000

    fixes and updates related to recent testing on apu11

--- benchmark-scripts/ann/benchmark.py
+++ benchmark-scripts/ann/benchmark.py
@@ -11,12 +11,13 @@ if __name__ == '__main__':
     ef_array = [64, 128, 256, 512]
 
     benchmark_file_array = [
-        ['deep-image-96-angular.hdf5', 'cosine'],
-        ['mnist-784-euclidean.hdf5', 'l2-squared'],
-        ['gist-960-euclidean.hdf5', 'l2-squared'],
-        ['glove-25-angular.hdf5', 'cosine']
+        ['deep-1M.hdf5','cosine']
+#        ['deep-image-96-angular.hdf5', 'cosine'],
+#        ['mnist-784-euclidean.hdf5', 'l2-squared'],
+#        ['gist-960-euclidean.hdf5', 'l2-squared'],
+#        ['glove-25-angular.hdf5', 'cosine']
     ]   
  
     # Starts the actual benchmark, prints "completed" when done
-    print("bm file", benchmark_file_array)
+    #print("bm file", benchmark_file_array)
     run_the_benchmarks(weaviate_url, CPUs, efConstruction_array, maxConnections_array, ef_array, benchmark_file_array)
