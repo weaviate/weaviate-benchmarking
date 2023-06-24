d886a5c9b23fb9ecf22d57d8666fe065ed4ccf70
George Williams <george.williams@gmail.com>
2023-06-24 15:59:34 +0000

    sync before run on apu12

--- benchmark-scripts/ann/benchmark.py
+++ benchmark-scripts/ann/benchmark.py
@@ -6,12 +6,12 @@ if __name__ == '__main__':
     # variables
     weaviate_url = 'http://weaviate:8084'
     CPUs = 32
-    efConstruction_array = [64, 128]
-    maxConnections_array = [16, 32]
+    efConstruction_array = [64] #, 128]
+    maxConnections_array = [16] #, 32]
     ef_array = [64, 128, 256, 512]
 
     benchmark_file_array = [
-        ['deep-10M.hdf5','cosine']
+        ['deep-5M.hdf5','cosine']
 #        ['deep-image-96-angular.hdf5', 'cosine'],
 #        ['mnist-784-euclidean.hdf5', 'l2-squared'],
 #        ['gist-960-euclidean.hdf5', 'l2-squared'],
