327c283375f8e75b1a4f2c5505f69958b22be308
Stefan Bogdan <stefanbogdan180394@gmail.com>
2024-07-10 11:57:42 +0200

    add CICD pipeline

---
+++ cicd/push_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -eou pipefail
+
+DOCKER_REPO="semitechnologies/weaviate-benchmarker"
+
+function main() {
+  init
+  echo "git ref type is $GITHUB_REF_TYPE"
+  echo "git ref name is $GITHUB_REF_NAME"
+
+  push_tag
+}
+
+function init() {
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  docker buildx create --use
+  docker build -t "$DOCKER_REPO" .
+}
+
+function push_tag() {
+  if [ ! -z "$GITHUB_REF_NAME" ] && [ "$GITHUB_REF_TYPE" == "tag" ]; then
+    tag_git="$DOCKER_REPO:$GITHUB_REF_NAME"
+    tag_latest="$DOCKER_REPO:latest"
+
+    echo "Tag & Push $tag_latest, $tag_git"
+    docker buildx build --platform=linux/arm64,linux/amd64 \
+      --push \
+      --tag "$tag_git" \
+      --tag "$tag_latest" \
+      .
+  fi
+}
+
+main
