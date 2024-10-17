a454f5b3b62abe41cf9d6d22975f6a1844384c39
Stefan Bogdan <stefanbogdan180394@gmail.com>
2024-10-17 13:40:37 +0200

    restore visualize.py

--- benchmarker/visualize.py
+++ benchmarker/visualize.py
@@ -1,102 +1,44 @@
-#!/usr/bin/env python3
-
-import os
-import glob
-import json
-import argparse
 import seaborn as sns
-import matplotlib.ticker as tkr
 import matplotlib.pyplot as plt
 import pandas as pd
 
-efConstruction = 256
-maxConnections = 32
+import os, glob
+import json
 
 
-def custom_filter(row):
-    if row['limit'] == 100 and row['ef'] < 90:
-        return False
-    return True 
+datapoints = []
+
+path = "./results"
+for filename in glob.glob(os.path.join(path, "*.json")):
+    with open(os.path.join(os.getcwd(), filename), "r") as f:
+        parsed = json.loads(f.read())
+        datapoints += parsed
+df = pd.DataFrame(datapoints)
+df = df.sort_values(by=['run_id'], ascending=False)
+dataset = df["dataset_file"].iloc[0]
 
 
-def get_datapoints(dataset:str, path: str):
-    datapoints = []
-    for filename in glob.glob(os.path.join(path, "*.json")):
-        with open(os.path.join(os.getcwd(), filename), "r") as f:
-            parsed = json.loads(f.read())
-            datapoints += parsed[1:]
-    df = pd.DataFrame(datapoints)
-    return df[
-        (df["dataset_file"] == dataset)     # filter for a specific dataset
-        & (df['run'] == "hnsw")              # remove PQ/BQ/SQ results
-        & (df["maxConnections"] == maxConnections)
-        & (df["efConstruction"] == efConstruction)
-        & (df.apply(custom_filter, axis=1))
-    ]
+#df = df[df["after_restart"] == "false"]
+
+sns.set_theme()
+plot = sns.relplot(
+    height=7,
+    aspect=1.2,
+    data=df,
+    markers=True,
+    kind="line",
+    x="recall",
+    y="qps",
+    hue="run_id",
+    style="run_id"
+    # style="cloud_provider",
+    # style="shards",
+    # size="size",
+)
+
+plot.set(title=f"{dataset} Query Performance")
 
 
-def create_plot(results_df: pd.DataFrame):
+plt.savefig("output.png", bbox_inches='tight')
 
-    dataset = results_df["dataset_file"].iloc[0]
-
-    sns.set_theme(
-        style='whitegrid',
-        font_scale=1.2,
-        rc={
-            # 'axes.grid': True,
-            # 'savefig.transparent': True,
-            # 'axes.facecolor': color,
-            # 'figure.facecolor': color,
-            # 'axes.edgecolor': color,
-            # 'grid.color': color,
-            # 'ytick.labelcolor': color,
-            # 'xtick.labelcolor': color,
-            }
-    )
-    plot = sns.relplot(
-        linewidth=3,
-        height=7,
-        aspect=1.5,
-        marker="o",
-        dashes=False,
-        data=results_df,
-        kind="line",
-        x="recall",
-        y="qps",
-        hue="limit",
-        style="limit",
-        palette=["b", "g"],
-    )
-    plot.set_axis_labels(
-        x_var="Recall, [%]",
-        y_var="QPS",
-    )
-    plot.figure.subplots_adjust(top=0.85)
-    plot.figure.suptitle(
-        f"Query Performance, {dataset}",
-        weight="bold",
-        
-    )
-    sns.move_legend(
-        plot,
-        "lower center",
-        bbox_to_anchor=(.5, .84),
-        ncol=3,
-        title="Limit: ",
-        frameon=False,
-    )
-
-
-    plot.axes[0][0].get_xaxis().set_major_formatter(tkr.FuncFormatter(lambda x, _: f'{x*100:.0f}'))
-    plot.axes[0][0].get_yaxis().set_major_formatter(tkr.StrMethodFormatter('{x:,.0f}'))
-    plt.savefig(f"{dataset.split('.')[0]}.png", bbox_inches='tight')
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Collate ann results into markdown tables.")
-    parser.add_argument('-d', '--dataset', required=True, help="The dataset file to filter by")
-    parser.add_argument('-r', '--results', default="./results", help="The directory containing benchmark results")
-    args = parser.parse_args()
-
-    create_plot(
-        get_datapoints(args.dataset, os.path.expanduser(args.results)),
-    )
+print(df["recall"].idxmax)
